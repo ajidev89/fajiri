6d81d85153a53ecadcad766b19c07ce85489738f
Ajidev <ajidagba21@gmail.com>
2026-03-08 23:49:20 +0100

    feat: build

--- app/Http/Repository/PreferenceRepository.php
+++ app/Http/Repository/PreferenceRepository.php
@@ -9,7 +9,15 @@ class PreferenceRepository implements PreferenceRepositoryInterface
 {
     public function getForUser($userId)
     {
-        return Preference::firstOrCreate(['user_id' => $userId]);
+        return Preference::firstOrCreate(['user_id' => $userId,
+        'notification_sound' => true,
+        'auto_update_software' => true,
+        'community_updates' => true,
+        'project_updates' => true,
+        'event_updates' => true,
+        'receive_payment_confirmation' => true,
+        'membership_status_updates' => true,
+    ]);
     }
 
     public function updateForUser($userId, array $data)
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,6 +25,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
+        $exceptions->shouldRenderJsonWhen(fn ($request, $e) => true);
         $exceptions->render(function (Throwable $exception, $request) {
                 
                 if ($exception instanceof ValidationException) {
