e6845dd1aaab9ed4aded0dde8d8c07c368692e9d
Ajidev <ajidagba21@gmail.com>
2026-04-07 15:24:10 +0100

    transaction

---
+++ app/Http/Resources/Transaction/TransactionResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\Transaction;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TransactionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'amount' => $this->amount,
+            'type' => $this->type,
+            'currency' => $this->currency,
+            'description' => $this->description,
+            'reference' => $this->reference,
+            'status' => $this->status,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -25,6 +26,13 @@ class Wallet extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function currency(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => optional($this->user->country)->currency,
+        );
     }
 
     public function transactions(): HasMany
