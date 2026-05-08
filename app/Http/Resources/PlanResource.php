eeb7bb0a8b98f1063d5033efeeb6b9efa1ab061c
Ajidev <ajidagba21@gmail.com>
2026-05-08 23:35:48 +0100

    Plan

--- app/Http/Resources/PlanResource.php
+++ app/Http/Resources/PlanResource.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\URL;
+
 
 class PlanResource extends JsonResource
 {
@@ -27,6 +27,7 @@ class PlanResource extends JsonResource
             'features' => $this->features,
             'status' => $this->status,
             'stripe_price_id' => $this->stripe_price_id,
+            'stripe_product_id' => $this->stripe_product_id,
             'paystack_plan_code' => $this->paystack_plan_code,
         ];
     }
